1a84cee826613ca5568bea59280dcebb0d7e8cda
m8dhouse <46300801+m8dhouse@users.noreply.github.com>
2020-07-12 12:40:25 +0200

    small changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
+/*
+Microview UVC timer
+GitHub: M8dhouse
+11/7/2020
+*/
 #include <MicroView.h>		// include MicroView library
 
 int relayPin = 2;				  // set relayPin as pin 2 of Arduino
-int startPinSwitch = 5;   //push button switch 
+int startPinSwitch = 5;   // push button switch 
 int coverReedSwitch = 3;  // reed switch in cover box
 int buzzerPin = 6;        // buzzer pin
 int timeLeftOver = 0;     // timeLeftOver when interrrupted
-int timerstart = 15;      // UVC exposure in seconds
-                          // UVC exposure in minutes => change line 68 for loop
+int timerValue = 15;      // UVC exposure in seconds
+                          // UVC exposure in minutes => change line 79 for loop
+String versionUVC = "1.0";
 MicroViewWidget *widget;
 
 void endUVC()
@@ -16,41 +22,41 @@ void endUVC()
   uView.setCursor(0,25);		
 	uView.print("Finished   ");
 	uView.display();
-  digitalWrite(buzzerPin, LOW);    // turn the buzzer on 
+  digitalWrite(buzzerPin, HIGH);    // turn the buzzer on 
   delay(1000);
-  digitalWrite(buzzerPin, HIGH);   // turn the buzzer off
+  digitalWrite(buzzerPin, LOW);   // turn the buzzer off
   delay(250);
-  digitalWrite(buzzerPin, LOW);    // turn the buzzer on 
+  digitalWrite(buzzerPin, HIGH);    // turn the buzzer on 
   delay(500);
-  digitalWrite(buzzerPin, HIGH);   // turn the buzzer off  
+  digitalWrite(buzzerPin, LOW);   // turn the buzzer off  
   delay(250);
-  digitalWrite(buzzerPin, LOW);    // turn the buzzer on 
+  digitalWrite(buzzerPin, HIGH);    // turn the buzzer on 
   delay(250);
-  digitalWrite(buzzerPin, HIGH);   // turn the buzzer off
+  digitalWrite(buzzerPin, LOW);   // turn the buzzer off
   delay(1000);
 }
 
 
-int boxOpened()
+void boxOpened()
 {
 
   while (digitalRead(coverReedSwitch) == HIGH)
   {
     digitalWrite(relayPin, LOW);    // turn the RELAY off
     uView.setCursor(0,0);		
-    uView.print("Close     ");
-    uView.setCursor(0,10);		
-    uView.print("cover     ");
+    uView.print("Close lid ");
     uView.display();
   }
 
   uView.setCursor(0,0);			    
-  uView.print("Radiating   ");			
-  uView.setCursor(0,10);
-  uView.print("Don't open   ");
-  uView.display();				      
+  uView.print("Radiating   ");		
+  uView.display();		
+  digitalWrite(buzzerPin, HIGH);    // turn the buzzer on 
+  delay(250);
+  digitalWrite(buzzerPin, LOW);   // turn the buzzer off	
+  delay(2000);    				// wait       
   digitalWrite(relayPin, HIGH); // turn the RELAY ON 
-  return timeLeftOver;
+
 }
 
 void startUVC ()
@@ -60,33 +66,38 @@ void startUVC ()
 	uView.print("DANGER    ");		
   uView.setCursor(0,10);
   uView.print("UV-C !!   ");
-	uView.display();				
+	uView.display();
   delay(2000);    				// wait 
-  widget= new MicroViewGauge(40,30,0,timerstart*2);// draw Gauge widget at x=32,y=30,min=0, max=100
+  uView.clear(PAGE);			  // clear page	
+  widget= new MicroViewGauge(40,30,0,timerValue);// draw Gauge widget at x=32,y=30,min=0, max=100
   uView.setCursor(0,0);		
 	uView.print("Radiating ");			
-  uView.setCursor(0,10);
-  uView.print("Don't open");
-	uView.display();				
+	uView.display();			
+  digitalWrite(buzzerPin, HIGH);    // turn the buzzer on 
+  delay(250);
+  digitalWrite(buzzerPin, LOW);   // turn the buzzer off	
   delay(2000);    				// wait 
 
-  while (digitalRead(coverReedSwitch) == LOW)
+  while (digitalRead(coverReedSwitch) == LOW)  // while cover closed
   {
     digitalWrite(relayPin, HIGH);   // turn the RELAY ON 
-    for(int i=timerstart*2; i>0;i--) {
+    // testing seconds for(int i=timerValue*2; i>0;i--) {
 
-    // minutes for(int i=timerstart*120; i>0;i--) {
+   for(int i=timerValue*120; i>0;i--) {
 
       if (digitalRead(coverReedSwitch) == HIGH) boxOpened();
       timeLeftOver = i;       // save time left over in var
-      widget->setValue(i);    // give a value to widget
-      uView.display();        // display current page buffer
+      if ( (i % 120) == 0) { 
+        widget->setValue(i/120);    // give a value to widget
+        uView.display();        // display current page buffer 
+      }
+      
       delay(500);    				  // wait for half a second (hence the 120 -> 2*60 for a minute) 
     }
-   break;                     // detected reed switch opened
+   break;                     // time done - reed switch still closed but time is done
   };
  
-  endUVC();                   // time done and reed switch closed
+  endUVC();                   // end procedure
 }
 
 
@@ -96,7 +107,7 @@ void setup() {
 
 	pinMode(relayPin, OUTPUT);	      // initialize the digital pin as an output.
   pinMode(buzzerPin, OUTPUT);	      // initialize the digital pin as an output.
-  digitalWrite(buzzerPin, HIGH);    // turn the buzzer off 
+  digitalWrite(buzzerPin, LOW);    // turn the buzzer off 
 
 	pinMode(startPinSwitch, INPUT_PULLUP);	// initialize the digital pin
 	pinMode(coverReedSwitch, INPUT_PULLUP);	// initialize the digital pin 
@@ -110,6 +121,8 @@ void loop() {
     digitalWrite(relayPin, LOW);              // turn the RELAY off 			
     uView.setCursor(0,0);		
     uView.print("Waiting   ");
+    uView.setCursor(40,40);	
+    uView.print("v" + versionUVC);
     uView.display();
 
   }
